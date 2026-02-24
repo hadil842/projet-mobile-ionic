3eed233eef871f169d64fc9e635484034e36cc24
hadil <hadilkharroubi85@gmail.com>
2026-02-24 11:22:35 +0100

    navbar

--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
 
+import { addIcons } from 'ionicons';
+import { home, } from 'ionicons/icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  imports: [IonIcon,IonButton],
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent  implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {}
+  }
 
 }
+
+
+
